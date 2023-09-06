b31cdf0ce1c145b381452b719b62e7731371a64f
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-06 11:47:22 -0500

    Add CLI shell.

--- src/Jpfulton.AzureAuditCli/Program.cs
+++ src/Jpfulton.AzureAuditCli/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Jpfulton.AzureAuditCli.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console.Cli;
+
+var services = new ServiceCollection();
+
+var registrar = new TypeRegistrar(services);
+
+var app = new CommandApp(registrar);
+
+app.Configure(config =>
+{
+    config.SetApplicationName("azure-price");
+
+#if DEBUG
+    config.PropagateExceptions();
+#endif
+
+    config.ValidateExamples();
+});
+
+return await app.RunAsync(args);
