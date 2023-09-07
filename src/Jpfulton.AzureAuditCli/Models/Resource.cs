89700d4ffe7831ad98aacf587f442f920b2ba40f
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-07 18:22:15 -0500

    Introduce concept of resource references.

--- src/Jpfulton.AzureAuditCli/Models/Resource.cs
+++ src/Jpfulton.AzureAuditCli/Models/Resource.cs
@@ -1,10 +1,8 @@
 namespace Jpfulton.AzureAuditCli.Models;
 
-public class Resource
+public class Resource : ResourceRef
 {
-  public string Id { get; set; } = string.Empty;
   public string ResourceType { get; set; } = string.Empty;
-  public string ResourceGroup { get; set; } = string.Empty;
   public string Name { get; set; } = string.Empty;
   public string Location { get; set; } = string.Empty;
   public Dictionary<string, string> Tags { get; set; } = new();
