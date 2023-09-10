9efeeaaca4047d70d7aaba6ac1076c349ffa37e6
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-10 13:48:40 -0500

    Initial markdown output draft.

--- src/Jpfulton.AzureAuditCli/OutputFormatters/MarkdownOutputFormatter.cs
+++ src/Jpfulton.AzureAuditCli/OutputFormatters/MarkdownOutputFormatter.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+using System.Text;
 using Jpfulton.AzureAuditCli.Commands;
 using Jpfulton.AzureAuditCli.Commands.Resources;
 using Jpfulton.AzureAuditCli.Commands.Subscriptions;
@@ -35,6 +37,65 @@ public class MarkdownOutputFormatter : BaseOutputFormatter
         Dictionary<Subscription, Dictionary<ResourceGroup, Dictionary<Resource, List<IRuleOutput>>>> data
         )
     {
+        var output = new StringBuilder();
 
+        output.AppendLine("# Audit Rule Outputs");
+        output.AppendLine();
+        output.AppendLine($"> Rendered on: {DateTime.Now.ToString("f", CultureInfo.CreateSpecificCulture("en-US"))}");
+        output.AppendLine($"> Using command: azure-audit {commandContext.Name}");
+        output.AppendLine("> Resource groups and resources without rule findings will be omitted.");
+        output.AppendLine();
+
+        data.Keys
+            .OrderBy(s => s.DisplayName)
+            .ToList()
+            .ForEach(subscription =>
+        {
+            output.AppendLine($"## {subscription.DisplayName}");
+            output.AppendLine();
+
+            data[subscription].Keys
+                .Where(rg => data[subscription][rg].Values.Any(l => l.Count > 0))
+                .OrderBy(rg => rg.Name)
+                .ToList()
+                .ForEach(resourceGroup =>
+            {
+                output.AppendLine($"### {resourceGroup.Name}");
+                output.AppendLine();
+
+                output.AppendLine("|---|---|---|---|");
+                output.Append("| Resource Type ");
+                output.Append("| Name ");
+                output.Append("| Level ");
+                output.Append("| Message ");
+                output.Append("|\n");
+
+                data[subscription][resourceGroup].Keys
+                    .Where(r => data[subscription][resourceGroup][r].Count > 0)
+                    .OrderBy(r => r.ResourceType)
+                    .ThenBy(r => r.Name)
+                    .ToList()
+                    .ForEach(resource =>
+                {
+                    data[subscription][resourceGroup][resource]
+                        .OrderByDescending(o => o.Level)
+                        .ThenBy(o => o.Message)
+                        .ToList()
+                        .ForEach(ruleOutput =>
+                    {
+                        output.Append($"| *{resource.ResourceType}* ");
+                        output.Append($"| **{resource.Name}** ");
+                        output.Append($"| [{Enum.GetName(ruleOutput.Level)}] ");
+                        output.Append($"| {ruleOutput.Message} ");
+                        output.Append("|\n");
+                    });
+                });
+
+                output.AppendLine();
+            });
+        });
+        output.AppendLine();
+
+        Console.WriteLine(output.ToString());
     }
 }
