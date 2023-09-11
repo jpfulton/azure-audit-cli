378cfc278cb39145f01c6bf2d2a4092ceca01f4f
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-10 22:27:16 -0500

    Finish draft of network access rule.

--- src/Jpfulton.AzureAuditCli/Rules/Storage/StorageAccounts/NetworkAccessRule.cs
+++ src/Jpfulton.AzureAuditCli/Rules/Storage/StorageAccounts/NetworkAccessRule.cs
@@ -25,6 +25,56 @@ public class NetworkAccessRule : IRule<StorageAccount>
                 resource
             ));
         }
+        else if (
+            !resource.AllowBlobPublicAccess &&
+            resource.NetworkAcls != null &&
+            !resource.NetworkAcls.Bypass.Equals("None") &&
+            resource.NetworkAcls.DefaultAction == NetworkAclAction.Deny &&
+            resource.NetworkAcls.IpRulesCount == 0 &&
+            resource.NetworkAcls.IpV6RulesCount == 0 &&
+            resource.NetworkAcls.VirtualNetworkRulesCount == 0 &&
+            resource.PrivateEndpointConnectionsCount == 0
+            )
+        {
+            outputs.Add(new DefaultRuleOutput(
+                Level.Warn,
+                "Storage account is completely inaccessible to all networks. Only Azure services are provided access.",
+                resource
+            ));
+        }
+        else if (
+            !resource.AllowBlobPublicAccess &&
+            resource.NetworkAcls != null &&
+            resource.NetworkAcls.DefaultAction == NetworkAclAction.Deny
+            )
+        {
+            if (resource.NetworkAcls.IpRulesCount > 0)
+            {
+                outputs.Add(new DefaultRuleOutput(
+                    Level.Info,
+                    $"Storage account is accessible via {resource.NetworkAcls.IpRulesCount} IP rule(s).",
+                    resource
+                ));
+            }
+
+            if (resource.NetworkAcls.IpV6RulesCount > 0)
+            {
+                outputs.Add(new DefaultRuleOutput(
+                    Level.Info,
+                    $"Storage account is accessible via {resource.NetworkAcls.IpV6RulesCount} IPv6 rule(s).",
+                    resource
+                ));
+            }
+
+            if (resource.NetworkAcls.VirtualNetworkRulesCount > 0)
+            {
+                outputs.Add(new DefaultRuleOutput(
+                    Level.Info,
+                    $"Storage account is accessible via {resource.NetworkAcls.VirtualNetworkRulesCount} virtual network rule(s).",
+                    resource
+                ));
+            }
+        }
 
         return outputs;
     }
